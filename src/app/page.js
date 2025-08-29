4a49b1b2e811235efee23c3d73309252514b396e
oladipotaiwo <oladipotaiwo2019@gmail.com>
2025-08-29 11:20:30 +0100

    changed text colors

--- src/app/page.js
+++ src/app/page.js
@@ -90,7 +90,7 @@ export default function HomePage() {
                     <h2 className="text-lg font-semibold text-gray-700">
                       {item.fullName || item.name}
                     </h2>
-                    <p className="text-gray-500">
+                    <p className="text-gray-600">
                       House: {item.hogwartsHouse || "Unknown"}
                     </p>
                     {item.image && (
